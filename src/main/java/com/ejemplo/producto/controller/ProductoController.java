3bc3f321780dfb83e33e51905aca6d5a02f7f546
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-02 17:14:59 -0500

    commit - ProductoController .

--- src/main/java/com/ejemplo/producto/controller/ProductoController.java
+++ src/main/java/com/ejemplo/producto/controller/ProductoController.java
@@ -1,4 +1,28 @@
 package com.ejemplo.producto.controller;
 
+import com.ejemplo.producto.entity.Producto;
+import com.ejemplo.producto.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/productos")
 public class ProductoController {
+
+    @Autowired
+    ProductoService service;
+
+    @PostMapping
+    public ResponseEntity<String> guardar(
+            @RequestBody Producto producto) {
+
+        service.guardar(producto);
+
+        return ResponseEntity.ok(
+                "producto registrado correctamente");
+    }   
 }
