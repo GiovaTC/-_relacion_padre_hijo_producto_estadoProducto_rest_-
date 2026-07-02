c2f0d1fbd7c2ede06ec5ecf523da16aa17b608b9
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-02 16:47:23 -0500

    commit - EstadoProducto / .

--- src/main/java/com/ejemplo/producto/entity/EstadoProducto.java
+++ src/main/java/com/ejemplo/producto/entity/EstadoProducto.java
@@ -1,4 +1,22 @@
 package com.ejemplo.producto.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "estado_producto")
 public class EstadoProducto {
-}
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String estado;
+    private LocalDate fechaRegistro;
+    private String observacion;
+    private String responsable;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+}   
